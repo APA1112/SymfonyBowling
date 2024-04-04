4f8c4e05e423511a04d34241e5cc4bb7ad893989
Alejandro Paton <alejandropatonaraujo2@gmail.com>
2024-04-04 10:22:34 +0000

    CineBundle finished

--- src/Bundles/ContabilidadBundle/Controller/ContabilidadController.php
+++ src/Bundles/ContabilidadBundle/Controller/ContabilidadController.php
@@ -12,17 +12,17 @@ class ContabilidadController extends AbstractController{
         return $this->render('@Contabilidad/contabilidad.html.twig');
     }
 
-    #[Route('/contabilidad/section1', name:'section1')]
+    #[Route('/contabilidad/section1', name:'contabilidad_section1')]
     public function section1() : Response{
         return $this->render('@Contabilidad/section1.html.twig');
     }
     
-    #[Route('/contabilidad/section2', name:'section2')]
+    #[Route('/contabilidad/section2', name:'contabilidad_section2')]
     public function section2() : Response{
         return $this->render('@Contabilidad/section2.html.twig');
     }
 
-    #[Route('/contabilidad/section3', name:'section3')]
+    #[Route('/contabilidad/section3', name:'contabilidad_section3')]
     public function section3() : Response{
         return $this->render('@Contabilidad/section3.html.twig');
     }
