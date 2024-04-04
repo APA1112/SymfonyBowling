d334ba650179e5ee24991012cc1110d5804b530d
Alejandro Paton <alejandropatonaraujo2@gmail.com>
2024-04-04 09:37:20 +0000

    Finished ContabilidadBundle

--- src/ContabilidadBundle/Controller/ContabilidadController.php
+++ src/ContabilidadBundle/Controller/ContabilidadController.php
@@ -11,4 +11,19 @@ class ContabilidadController extends AbstractController{
     public function index():Response{
         return $this->render('@Contabilidad/contabilidad.html.twig');
     }
+
+    #[Route('/contabilidad/section1', name:'section1')]
+    public function section1() : Response{
+        return $this->render('@Contabilidad/section1.html.twig');
+    }
+    
+    #[Route('/contabilidad/section2', name:'section2')]
+    public function section2() : Response{
+        return $this->render('@Contabilidad/section2.html.twig');
+    }
+
+    #[Route('/contabilidad/section3', name:'section3')]
+    public function section3() : Response{
+        return $this->render('@Contabilidad/section3.html.twig');
+    }
 }
