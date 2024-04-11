4666e143f45c2db703af69a92058bbb365164394
Alejandro Paton <alejandropatonaraujo2@gmail.com>
2024-04-11 09:29:14 +0000

    Getting back

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,7 +13,7 @@ class MainController extends AbstractController{
         // Verificar si hay un usuario autenticado
         if ($security->getUser()) {
             // Si hay un usuario autenticado, redirigir a la pantalla principal
-            return $this->render('vue.html.twig');
+            return $this->render('Main/main.html.twig');
         } else {
             // Si no hay un usuario autenticado, redirigir a la pantalla de inicio de sesión
             return $this->redirectToRoute('login_App');
